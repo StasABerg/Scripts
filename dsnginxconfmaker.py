29a936c48413ce2ad0276e7f544067709982d123
Stanislav Amaral Berg <114340468+StasABerg@users.noreply.github.com>
2023-05-04 10:04:44 +0300

    Create dsnginxconfmaker.py
    Makes nginx conf files with/without www from a csv file.

---
+++ dsnginxconfmaker.py
@@ -0,0 +1,37 @@
+import csv
+
+with open('list.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    sites = list(reader)
+
+# Generate a configuration file for each site
+for site in sites:
+    sitename = site['sitename']
+    www_option = site['www_option']
+    if www_option == "www":
+        prefix = "www."
+        redirect_site = sitename
+    else:
+        prefix = ""
+        redirect_site = f"www.{sitename}"
+
+
+    with open(f{sitename}.conf', 'w') as f:
+        f.write(f'server {{\n')
+        f.write(f'    listen 443 ssl http2;\n')
+        f.write(f'    ssl_certificate /etc/letsencrypt/live/{sitename}/fullchain.pem;\n')
+        f.write(f'    ssl_certificate_key /etc/letsencrypt/live/{sitename}/privkey.pem;\n')
+        f.write(f'    server_name {sitename} www.{sitename};\n')
+        f.write(f'    root /var/www/{sitename};\n')
+        f.write(f'    if ($host = "{redirect_site}") {{\n')
+        f.write(f'        return 301 https://{prefix}{sitename}$request_uri;\n')
+        f.write(f'    }}\n\n')
+        f.write(f'}}\n\n')
+
+        f.write(f'server {{\n')
+        f.write(f'    listen 80;\n')
+        f.write(f'    server_name {sitename} www.{sitename};\n\n')
+        f.write(f'    location = / {{\n')
+        f.write(f'        return 301 https://{prefix}{sitename}$request_uri;\n')
+        f.write(f'    }}\n\n')
+        f.write(f'}}\n\n')
