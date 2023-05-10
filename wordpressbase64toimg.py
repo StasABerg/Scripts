6787e020d8ad75c45754cf1939b1d60d785a189e
Stanislav Amaral Berg <114340468+StasABerg@users.noreply.github.com>
2023-05-10 09:52:35 +0300

    Update wordpressbase64toimg.py
    Fixed issue where downloads/uploads image again to put in featured image.

--- wordpressbase64toimg.py
+++ wordpressbase64toimg.py
@@ -41,10 +41,10 @@ def get_all_posts():
 
 post_data = get_all_posts()
 
-#loop through posts and find base64
+# loop through posts and find base64
 for post in post_data:
     post_id = post['id']
-    print(f'Start processing post ID: {post_id}') 
+    print(f'Start processing post ID: {post_id}')
     response = requests.get(f'{site_url}/wp-json/wp/v2/posts/{post_id}', auth=auth)
     if response.status_code != 200:
         print('Error retrieving post:', response.text)
@@ -53,6 +53,7 @@ for post in post_data:
     post_data = response.json()
     post_content = post_data['content']['rendered']
     matches = re.findall(r'src="data:image/([^;]+);base64,([^"]+)"', post_content)
+    first_image_attachment_id = None  #default none for featured image
 
     for match in matches:
         image_type, image_data = match
@@ -66,7 +67,7 @@ for post in post_data:
         if len(image_data) < 10 or image_data[:2] not in (b'\xff\xd8', b'\x89P', b'BM', b'GIF'):
             print(f'Invalid image data for post ID: {post_id}. Image data: {image_data[:10]}...')
             continue
-       #turn patterned and rgba to img
+        #special cases for patterned and rgba images
         try:
             image = Image.open(BytesIO(image_data))
             if image.mode == 'P':
@@ -76,12 +77,12 @@ for post in post_data:
         except OSError:
             print(f'Error opening image for post ID: {post_id}. Base64 data: {image_data[:50]}...')
             continue
-
+        #make md5has filename
         image_hash = hashlib.md5(image_data).hexdigest()
         filename = f'{image_hash}.{image_type}'
-        temp_file = os.path.join('C:\\temp', filename) #save image locally
+        temp_file = os.path.join('C:\\temp', filename)  # save image locally
         image.save(temp_file)
-        #upload to media
+        #upload to wordpress library
         try:
             with open(temp_file, 'rb') as f:
                 files = {'file': (filename, f, f'image/{image_type}')}
@@ -94,7 +95,10 @@ for post in post_data:
                     print('Error uploading image:', media_data['errors'])
                     continue
                 attachment_id = response.json()['id']
-
+                # if first image, store attachment id for the featured image
+                if first_image_attachment_id is None:
+                    first_image_attachment_id = attachment_id
+                # replace old url with new url
                 new_url = media_data['source_url']
                 base64_str = match[1]
                 old_src = f'data:image/{image_type};base64,{base64_str}'
@@ -105,7 +109,8 @@ for post in post_data:
             print(f'Response body: {response.content}')
             continue
 
-            #update base64 url to imagefile url
+        
+        #update the post
         post_data['content'] = post_content
         response = requests.post(f'{site_url}/wp-json/wp/v2/posts/{post_id}',
                                  json=post_data,
@@ -115,49 +120,20 @@ for post in post_data:
             continue
 
         print(f'Post {post_id} updated with new image URL')
+           # update featured image with the first image
+        if first_image_attachment_id is not None:
+            post_data['featured_media'] = first_image_attachment_id
+            response = requests.post(f'{site_url}/wp-json/wp/v2/posts/{post_id}',
+                                     json=post_data,
+                                     auth=auth)
+            if response.status_code != 200:
+                print('Error updating post:', response.text)
+                continue
 
-        # remove files locally
+            print(f'Post {post_id} updated with new featured image')
+
+        else:
+            print(f'No valid base64 images found for post ID: {post_id}')
+        
         os.remove(temp_file)
-    #get post id for featured images
-    response = requests.get(f'{site_url}/wp-json/wp/v2/posts/{post_id}', auth=auth)
-    post_data = response.json()
-
-    #check if the post already has a featured image
-    if 'featured_media' in post_data and post_data['featured_media']:
-        print(f'Post {post_id} already has a featured image')
-        continue
-
-    #make 1st pic featured or that starts with http
-    for match in re.finditer(r'<img\s.*?src="(.*?)".*?>', post_content):
-        image_url = match.group(1)
-        if image_url.startswith('data:'):
-            continue
-        if not image_url.startswith('http'):
-            continue
-        try:
-            with requests.get(image_url, stream=True) as r:
-                r.raise_for_status()
-                response = requests.post(f'{site_url}/wp-json/wp/v2/media',
-                                         files={'file': ('image.jpg', r.raw, 'image/jpeg')},
-                                         auth=auth)
-                response.raise_for_status()
-                media_data = response.json()
-                attachment_id = media_data['id']
-                post_data['featured_media'] = attachment_id
-                response = requests.post(f'{site_url}/wp-json/wp/v2/posts/{post_id}',
-                                         json=post_data,
-                                         auth=auth)
-                response.raise_for_status()
-                print(f'Post {post_id} updated with new featured image')
-                break
-        except requests.exceptions.HTTPError as err:
-            print(f'Error updating featured image for post ID: {post_id}:', err.response.text)
-            break
-        except Exception as err:
-            print(f'Error updating featured image for post ID: {post_id}:', str(err))
-            break
-
-    if 'featured_media' not in post_data or not post_data['featured_media']:
-        print(f'No image found for post ID: {post_id}')
-
-    print(f'Finished processing post ID: {post_id}')  
+        print(f'Finished processing post ID: {post_id}')
